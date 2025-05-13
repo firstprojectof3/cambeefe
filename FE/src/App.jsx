e2e761b2b925fb51d451614b174bf2381036958d
digurri <did1613@naver.com>
2025-05-13 19:17:30 +0900

    Add files via upload

---
+++ FE/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import ChatWindow from './components/ChatWindow'
+import InputArea from './components/InputArea'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ChatWindow />
+      <InputArea />
+    </div>
+  )
+}
+
+export default App
