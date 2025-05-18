6e5b2c72aa21d72a1d24f15a9d3328220e62e111
digurri <did1613@naver.com>
2025-05-18 15:30:35 +0900

    Update ChatWindow.jsx
    server 연결 준비

--- FE/src/components/ChatWindow.jsx
+++ FE/src/components/ChatWindow.jsx
@@ -1,13 +1,6 @@
 import ChatBubble from "./ChatBubble";
 
-function ChatWindow() {
-    const messages = [
-        { sender: 'user', text: '안녕?' },
-        { sender: 'ai', text: '안녕하세요! 무엇을 도와드릴까요?' },
-        { sender: 'user', text: '오늘 공대 식단 알려줘' },
-        { sender: 'ai', text: '미소된장국, 잡곡밥, 오뎅볶음이 석식으로 준비되어 있습니다:)'}
-    ];
-
+function ChatWindow({ messages }) {
     return (
         <div style={{ padding: '10px' }}>
           {messages.map((msg, idx) => (
